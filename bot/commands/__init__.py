d3d5546268f25485134db7f032665da39c09038d
SnowFox-95 <321mts95@gmail.com>
2022-09-20 21:50:09 +0300

    part_3_1 keyboard_start

--- bot/commands/__init__.py
+++ bot/commands/__init__.py
@@ -1,9 +1,9 @@
 __all__ = ['register_user_commands', 'bot_commands']
 
 from aiogram import Router
-# from aiogram.dispatcher.filters.command import CommandStart
-from aiogram.filters import Command
+from aiogram.filters import Command  # from aiogram.dispatcher.filters.command import CommandStart
 from aiogram.filters.command import CommandStart
+
 from aiogram import F
 from bot.commands.help import help_cmd
 from bot.commands.start import start
--- bot/commands/start.py
+++ bot/commands/start.py
@@ -1,5 +1,22 @@
 from aiogram import types
+from aiogram.utils.keyboard import (
+    ReplyKeyboardBuilder, ReplyKeyboardMarkup, InlineKeyboardBuilder, InlineKeyboardMarkup, KeyboardButton,
+    InlineKeyboardButton, KeyboardButtonPollType
+)
 
 
 async def start(message: types.Message) -> None:
-    await message.answer('Привет')
+    menu_bulder = ReplyKeyboardBuilder()
+    menu_bulder.button(
+        text='Помощь'
+    )
+    menu_bulder.add(
+        KeyboardButton(text='Отправить контакт', request_contact=True)
+    )
+    menu_bulder.row(
+        KeyboardButton(text='Отправить голосование', request_poll=KeyboardButtonPollType())
+    )
+    await message.answer(
+        'Меню',
+        reply_markup=menu_bulder.as_markup()
+    )
