3b750b6bdc4d5c75a15a1b2a3b4d82e8c782f2da
SnowFox-95 <321mts95@gmail.com>
2022-09-20 23:00:13 +0300

    part_3_4_2 keyboard_callback

--- bot/commands/__init__.py
+++ bot/commands/__init__.py
@@ -7,19 +7,25 @@ from aiogram.filters import Command
 # endregion
 from aiogram.filters.command import CommandStart
 
+
 from aiogram import F
-from bot.commands.help import help_cmd, help_func, call_help_func
+from bot.commands.help import help_cmd, help_func, call_help_func, clear_call_help_func
 from bot.commands.start import start
 from bot.commands.bot_commands import bot_commands
 from bot.commands.settings import setting_command
 
 
 def register_user_commands(router: Router) -> None:
+    # region message_register
     # router.message.register(start, Command(commands=['start']))
     router.message.register(start, CommandStart())
     router.message.register(help_cmd, Command(commands=['help']))
     router.message.register(start, F.text == 'Старт')
     router.message.register(help_func, F.text == 'Помощь')
     router.message.register(setting_command, Command(commands=['settings']))
+    # endregion
 
+    # region callback_register
     router.callback_query.register(call_help_func, F.data == 'help')
+    router.callback_query.register(clear_call_help_func, F.data == 'clear')
+    # endregion
--- bot/commands/help.py
+++ bot/commands/help.py
@@ -1,6 +1,10 @@
 from aiogram import types
 from aiogram.filters import CommandObject
 
+# region Unused import
+# from aiogram.utils.keyboard import InlineKeyboardButton
+# endregion
+
 from bot.commands.bot_commands import bot_commands
 
 
@@ -24,8 +28,34 @@ async def help_func(message: types.Message):
     )
 
 
+# region callback from answer message
+#   async def call_help_func(call: types.CallbackQuery):
+#       return await message.answer(
+#           'Помощь и справка о боте \n'
+#           'Для того, чтобы получить информацию о команде используй /help <команда>'
+#       )
+# endregion
+
+# region callback from edit message
 async def call_help_func(call: types.CallbackQuery):
-    return await call.message.answer(
+    return await call.message.edit_text(
+        'Помощь и справка о боте \n'
+        'Для того, чтобы получить информацию о команде используй /help <команда>'
+        # region don't_working
+        # ,
+        #
+        # reply_markup=call.message.reply_markup.inline_keyboard.append(
+        #    [InlineKeyboardButton(text='Назад', callback_data='clear')])
+        # endregion
+    )
+
+
+# endregion
+
+# region callback clear_help_func
+async def clear_call_help_func(call: types.CallbackQuery):
+    await call.message.edit_text(
         'Помощь и справка о боте \n'
         'Для того, чтобы получить информацию о команде используй /help <команда>'
     )
+# endregion
