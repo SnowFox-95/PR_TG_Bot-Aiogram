f8ea2ee6b575c6febb91099cd21cee383bda52ea
SnowFox-95 <321mts95@gmail.com>
2022-09-21 23:50:08 +0300

    part_5_database_engine_p2

--- bot/db/__init__.py
+++ bot/db/__init__.py
@@ -1,1 +1,5 @@
+__all__ = ["baseModel", "create_async_engine", "get_session_maker", "proceed_schemas", "User"]
 
+from .base import baseModel
+from .engine import create_async_engine, get_session_maker, proceed_schemas
+from .user import User
--- bot/db/user.py
+++ bot/db/user.py
@@ -1,3 +1,5 @@
+import datetime
+
 from sqlalchemy import Column, Integer, VARCHAR, DATE
 from .base import baseModel
 
@@ -6,16 +8,16 @@ class User(baseModel):
     __tablename__ = 'users'
 
     # TELEGRAM_USER_ID
-    user_id = Column(Integer, unique=True, nullable=False)
+    user_id = Column(Integer, unique=True, nullable=False, primary_key=True)
 
     # TELEGRAM_USER_NAME
     username = Column(VARCHAR(32), unique=False, nullable=True)
 
     # Registration date
-    reg_date = Column()
+    reg_date = Column(DATE, default=datetime.date.today())
 
     # Last update date
-    upd_date = Column()
+    upd_date = Column(DATE, onupdate=datetime.date.today())
 
     def __str__(self) -> str:
         return f"<User:(self.user_id)>"
